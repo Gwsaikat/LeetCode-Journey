922d47fa27eb81f5c8dcc42ed401471c25c1152b
MrROBOT <116334379+Gwsaikat@users.noreply.github.com>
2026-09-17 19:24:29 +0530

    Time: 0 ms (100%), Space: 9.8 MB (48.41%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        unordered_map<char,int>mp;
-
-        for ( char ch : s) mp[ch]++;
-        for ( char ch : t ) mp[ch]--;
-
-        for ( const auto& [key , val ] : mp ){
-            if ( val != 0 ) return false;
+        if (s.length() != t.length()) return false; 
+        
+        vector<int> mp(26, 0);
+        
+        for (char ch : s) mp[ch - 'a']++;
+        for (char ch : t) mp[ch - 'a']--;
+        
+        for (int val : mp) {
+            if (val != 0) return false;
         }
-
+        
         return true;
     }
 };
